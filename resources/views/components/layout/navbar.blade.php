466800d4ba55eebb063d77252e9adc5f5f4224ca
akimanagabriel <akimanagabriel@gmail.com>
2026-07-14 18:30:28 +0200

    Remove notifications section from the navbar and comment out the settings link to streamline the user interface.

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -32,22 +32,6 @@
     </div>
 
     <div class="flex-none flex items-center gap-2">
-
-        {{-- Notifications --}}
-        <div class="dropdown dropdown-end">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle btn-sm">
-                <div class="indicator">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                    </svg>
-                    <span class="h-4 w-4 rounded-full text-white bg-primary indicator-item">3</span>
-                </div>
-            </div>
-            
-        </div>
-
         {{-- User menu --}}
         <div class="dropdown dropdown-end">
             <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar placeholder">
@@ -64,7 +48,6 @@
                     <span class="text-xs text-base-content/50">{{ Auth::user()->email ?? '' }}</span>
                 </li>
                 <li><a href="{{ route('admin.profile.index') }}">Profile</a></li>
-                <li><a href="{{ route('admin.settings.index') }}">Settings</a></li>
                 <div class="divider my-1"></div>
                 <li>
                     <a href="{{ route('logout') }}"
